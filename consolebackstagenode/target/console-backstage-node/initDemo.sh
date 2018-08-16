f0c6cdc8a1f278ba6e46c08b02941cf6463bff85
sunpengxx5 <1461692534@qq.com>
2018-08-16 17:41:49 +0800

    demo

--- consolebackstagenode/target/console-backstage-node/initDemo.sh
+++ consolebackstagenode/target/console-backstage-node/initDemo.sh
@@ -1,17 +1,17 @@
 #Manager services config
 managerServicesUrl=${managerServicesUrl//\//\\\/}
 managerServicesUrl=${managerServicesUrl//&/\\&}
-managerServicesConfigPath=/usr/local/tomcat/webapps/console-backstage-demo/WEB-INF/classes/config/managerservices.properties
+managerServicesConfigPath=/usr/local/tomcat/webapps/ROOT/WEB-INF/classes/config/managerservices.properties
 sed -i 's/\(managerServicesUrl=\).*/\1'$managerServicesUrl'/' $managerServicesConfigPath
 
 #Db services config
 dbServicesUrl=${dbServicesUrl//\//\\\/}
 dbServicesUrl=${dbServicesUrl//&/\\&}
-dbServicesConfigPath=/usr/local/tomcat/webapps/console-backstage-demo/WEB-INF/classes/config/dbservices.properties
+dbServicesConfigPath=/usr/local/tomcat/webapps/ROOT/WEB-INF/classes/config/dbservices.properties
 sed -i 's/\(hostAndPort=\).*/\1'$dbServicesUrl'/' $dbServicesConfigPath
 
 # Redis config
-redisConfigPath=/usr/local/tomcat/webapps/console-backstage-demo/WEB-INF/classes/config/redis.properties
+redisConfigPath=/usr/local/tomcat/webapps/ROOT/WEB-INF/classes/config/redis.properties
 sed -i 's/\(redis.host=\).*/\1'$redisHost'/' $redisConfigPath
 sed -i 's/\(redis.port=\).*/\1'$redisPort'/' $redisConfigPath
 sed -i 's/\(redis.passwd=\).*/\1'$redisPasswd'/' $redisConfigPath
