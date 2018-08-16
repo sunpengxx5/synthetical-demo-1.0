729b1aa7dd2bf98d28b8df845e9dd6409eafba1e
sunpengxx5 <1461692534@qq.com>
2018-08-16 17:22:24 +0800

    demo

--- managerconsumernode/src/main/webapp/index.jsp
+++ managerconsumernode/src/main/webapp/index.jsp
@@ -4,12 +4,7 @@
 %>
 <html>
 <body>
-<h1>综合Demo资料 -- Manager模块</h1>
-    <pre>
-        <br>
-        <a href="<%=bpath %>/doc/synchetical-demo-design.html"style="font-size:26px;">综合Demo结构与数据库设计</a>
-        <br>
-        <a href="<%=bpath %>/doc/manager-api.html"style="font-size:26px;">Manager Rest Api 汇总</a>
-    </pre>
+<h1>综合Demo资料 -- Manager consumer模块</h1>
+    <h1>暂无！</h1>
 </body>
 </html>
--- managerconsumernode/src/main/webapp/initDemo.sh
+++ managerconsumernode/src/main/webapp/initDemo.sh
@@ -1,23 +1,23 @@
 # DB services config
 dbUrl=${dbUrl//\//\\\/}
 dbUrl=${dbUrl//&/\\&}
-dbConfigPath=/usr/local/tomcat/webapps/manager-consumer-node/WEB-INF/classes/config/component/dbservice.properties
+dbConfigPath=/usr/local/tomcat/webapps/ROOT/WEB-INF/classes/config/component/dbservice.properties
 sed -i 's/\(hostAndPort=\).*/\1'$dbUrl'/' $dbConfigPath
 
 # Kafka config
-kafkaConfigPath=/usr/local/tomcat/webapps/manager-consumer-node/WEB-INF/classes/config/component/kafka.properties
+kafkaConfigPath=/usr/local/tomcat/webapps/ROOT/WEB-INF/classes/config/component/kafka.properties
 sed -i 's/\(bootstrap.servers=\).*/\1'$bootstrapServers'/' $kafkaConfigPath
 sed -i 's/\(topic=\).*/\1'$topic'/' $kafkaConfigPath
 sed -i 's/\(group.id=\).*/\1'$groupId'/' $kafkaConfigPath
 
 # Kafka client config
-kafkaClientConfigPath=/usr/local/tomcat/webapps/manager-consumer-node/WEB-INF/classes/dms_kafka_client_jaas.conf
+kafkaClientConfigPath=/usr/local/tomcat/webapps/ROOT/WEB-INF/classes/dms_kafka_client_jaas.conf
 sed -i 's/\(access_key=\).*/\1\"'$accessKey'\"/' $kafkaClientConfigPath
 sed -i 's/\(secret_key=\).*/\1\"'$secretKey'\"/' $kafkaClientConfigPath
 sed -i 's/\(project_id=\).*/\1\"'$projectId'\";/' $kafkaClientConfigPath
 
 # Redis config
-redisConfigPath=/usr/local/tomcat/webapps/manager-consumer-node/WEB-INF/classes/config/component/redis.properties
+redisConfigPath=/usr/local/tomcat/webapps/ROOT/WEB-INF/classes/config/component/redis.properties
 sed -i 's/\(redis.host=\).*/\1'$redisHost'/' $redisConfigPath
 sed -i 's/\(redis.port=\).*/\1'$redisPort'/' $redisConfigPath
 sed -i 's/\(redis.passwd=\).*/\1'$redisPasswd'/' $redisConfigPath
